61e92ad0cfbd8d3ad91a0b7508b67a8d5c0db925
Robin Spruytte <robin.spruytte646@gmail.com>
2019-02-22 17:36:00 +0100

    added Tile class for hex sprites

---
+++ demoGame/js/Tile.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+class Tile {
+    constructor(texture, scene, x, y, cubePosition, size){
+        this.position = {x: x, y: y};
+        this.cubePosition = cubePosition;
+        this.body = scene.add.sprite(x, y, texture).setDisplaySize(size, size);
+        scene.add.sprite(x, y, texture).setDisplaySize(size);
+    }
+
+    setTexture(texture) {
+        this.body.setTexture(texture);
+    }
+}
