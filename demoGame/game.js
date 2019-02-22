5e80e993d981c51849d4aae1f87a4c3b96564aa4
Robin Spruytte <robin.spruytte646@gmail.com>
2019-02-22 17:17:18 +0100

    added basic phaser files

---
+++ demoGame/game.js
@@ -0,0 +1,16 @@
+var config = {
+    type:Phaser.AUTO,
+    width : 1200,
+    height : 800,
+    parent: 'play',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: { y: 100 }
+        }
+    },
+    scene: [ main ]
+};
+
+var game = new Phaser.Game(config);
