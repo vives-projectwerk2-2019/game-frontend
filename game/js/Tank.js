59740f93bad952a9d20011f2c0bf93c94ed60d40
Robin Spruytte <robin.spruytte646@gmail.com>
2019-03-13 23:03:56 +0100

    made Tank inherit from HexMover class

--- game/js/Tank.js
+++ game/js/Tank.js
@@ -1,10 +1,6 @@
-class Tank {
-    constructor(tankTexture, scene, map, x, y, size) {
-        this.map = map;
-        this.currentTile = this.map.getTile({x: x, y: y});
-        this.sprite = scene.add.sprite(this.currentTile.body.x, this.currentTile.body.y, tankTexture).setDisplaySize(size,size);
-        this.currentPosition = this.updateCurrentPosition();
-        this.currentRotation = 1;
-        this.updateCurrentRotation();
+/*jshint esversion: 6 */
+class Tank extends HexMover {
+    constructor(texture, scene, map, x, y, size) {
+        super(texture, scene, map, x, y, size);
     }
 }
