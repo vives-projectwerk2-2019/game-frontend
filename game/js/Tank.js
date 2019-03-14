3ca66e31b627b0da68e651e41aaf010c18555f04
jasper debaene <jasperdebaene@hotmail.be>
2019-03-14 18:58:27 +0100

    implemented animations and scoreboard.

--- game/js/Tank.js
+++
@@ -1,6 +0,0 @@
-/*jshint esversion: 6 */
-class Tank extends HexMover {
-    constructor(texture, scene, map, x, y, size) {
-        super(texture, scene, map, x, y, size);
-    }
-}
--- scene switch/mainScreen.js
+++ scene switch/mainScreen.js
@@ -4,17 +4,28 @@ class main extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('tankblack','assets/tankblack.png');
-        this.load.image('tankblue','assets/tankblue.png');
-        this.load.image('tankcyan','assets/tankcyan.png');
-        this.load.image('tankgreen','assets/tankgreen.png');
-        this.load.image('tankgrey','assets/tankgrey.png');
-        this.load.image('tankpurple','assets/tankpurple.png');
-        this.load.image('tankred','assets/tankred.png');
-        this.load.image('tankyellow','assets/tankyellow.png');
+        //tanks
+        this.load.image('tankblack','assets/tanks/tankblack.png');
+        this.load.image('tankblue','assets/tanks/tankblue.png');
+        this.load.image('tankcyan','assets/tanks/tankcyan.png');
+        this.load.image('tankgreen','assets/tanks/tankgreen.png');
+        this.load.image('tankgrey','assets/tanks/tankgrey.png');
+        this.load.image('tankpurple','assets/tanks/tankpurple.png');
+        this.load.image('tankred','assets/tanks/tankred.png');
+        this.load.image('tankyellow','assets/tanks/tankyellow.png');
+        //worldgen
         this.load.image('tile','assets/Tiles/tileAutumn_tile.png');
         this.load.image('selectedTile','assets/Tiles/tileDirt_tile.png');
         this.load.image('background', 'assets/background.jpg');
+        //Animations
+        this.load.spritesheet('explosion', 'assets/animations/explosion.png', { frameWidth: 64, frameHeight: 64, endFrame: 9 });
+
+        this.load.spritesheet('eastbullet', 'assets/animations/eastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('westbullet', 'assets/animations/westbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('northeastbullet', 'assets/animations/northeastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('northwestbullet', 'assets/animations/northwestbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('southeastbullet', 'assets/animations/southeastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('southwestbullet', 'assets/animations/southwestbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });        
     }
 
     init () {
@@ -36,7 +47,7 @@ class main extends Phaser.Scene {
         this.setupFullScreen(this.background);
 
         this.map = new HexMap('tile', 'selectedTile', this, 22, 26, 60, 40,35);
-        this.tankblack = new Tank('tankblack', this, this.map, 1, 1, 45);
+        this.tankblack = new Tank('tankblack', this, this.map, 4, 4, 45);
         this.tankblue = new Tank('tankblue', this, this.map, 20, 1, 45);
         this.tankcyan = new Tank('tankcyan', this, this.map, 20, 20, 45);
         this.tankgreen = new Tank('tankgreen', this, this.map, 1, 10, 45);
@@ -45,6 +56,20 @@ class main extends Phaser.Scene {
         this.tankbred = new Tank('tankred', this, this.map, 10, 1, 45);
         this.tankyellow = new Tank('tankyellow', this, this.map, 20, 10, 45);
         this.map.selectTile(1,1);
+
+        this.nameText = this.add.text(10, 10, 'Name: Chickmagnet73', { font: '32px Arial', fill: '#000000' });
+        this.scoreText = this.add.text(10, 48, 'Score: 0', { font: '32px Arial', fill: '#000000' });
+        this.hitpointsText = this.add.text(10, 86, 'Hitpoints: 100', { font: '32px Arial', fill: '#000000' });
+
+        var explosion = {
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { frames: [ 0, 1, 2, 3, 4 ] }),
+            frameRate: 5,
+            repeat: -1
+        };
+
+        this.anims.create(explosion);
+        //this.add.sprite(300, 300, 'explosion').play('explode');
     }
 
 /*     update(delta){
@@ -98,6 +123,9 @@ class main extends Phaser.Scene {
         },this);
         this.input.keyboard.on('keyup_D', function(event){
             this.tankblack.turnRight();
+        },this);
+        this.input.keyboard.on('keyup_P', function(event){
+            this.add.sprite(300, 300, 'explosion').play('explode');
         },this);
 
         //controls tankblue
