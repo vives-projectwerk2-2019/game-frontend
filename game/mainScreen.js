845a2541f005a5636a561d6f6766a2251bd0cf9b
jasper debaene <jasperdebaene@hotmail.be>
2019-03-14 16:36:42 +0100

    tanks, animations ans scoreboard.

--- game/mainScreen.js
+++ game/mainScreen.js
@@ -5,17 +5,28 @@ class main extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('tankblack','assets/tankblack.png');
-        this.load.image('tankblue','assets/tankblue.png');
-        this.load.image('tankcyan','assets/tankcyan.png');
-        this.load.image('tankgreen','assets/tankgreen.png');
-        this.load.image('tankgrey','assets/tankgrey.png');
-        this.load.image('tankpurple','assets/tankpurple.png');
-        this.load.image('tankred','assets/tankred.png');
-        this.load.image('tankyellow','assets/tankyellow.png');
+        //Tanks
+        this.load.image('tankblack','assets/tanks/tankblack.png');
+        this.load.image('tankblue','assets/tanks/tankblue.png');
+        this.load.image('tankcyan','assets/tanks/tankcyan.png');
+        this.load.image('tankgreen','assets/tanks/tankgreen.png');
+        this.load.image('tankgrey','assets/tanks/tankgrey.png');
+        this.load.image('tankpurple','assets/tanks/tankpurple.png');
+        this.load.image('tankred','assets/tanks/tankred.png');
+        this.load.image('tankyellow','assets/tanks/tankyellow.png');
+        //worldgen
         this.load.image('tile','assets/Tiles/tileAutumn_tile.png');
         this.load.image('selectedTile','assets/Tiles/tileDirt_tile.png');
-        this.load.image('background', 'assets/background.jpg');
+        this.load.image('background','assets/background.jpg');
+        //Animations
+        this.load.spritesheet('explosion', 'assets/animations/explosion.png', { frameWidth: 64, frameHeight: 64, endFrame: 9 });
+
+        this.load.spritesheet('eastbullet', 'assets/animations/eastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('westbullet', 'assets/animations/westbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('northeastbullet', 'assets/animations/northeastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('northwestbullet', 'assets/animations/northwestbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('southeastbullet', 'assets/animations/southeastbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
+        this.load.spritesheet('southwestbullet', 'assets/animations/southwestbullet.png', { frameWidth: 32, frameHeight: 32, endFrame: 4 });
     }
 
     init () {
@@ -33,11 +44,10 @@ class main extends Phaser.Scene {
     create() {
         this.setupKeyBinds();
         this.background = this.add.image(1200/2, 800/2, 'background');
-
         this.setupFullScreen(this.background);
 
         this.map = new HexMap('tile', 'selectedTile', this, 22, 26, 60, 40,35);
-        this.tankblack = new Tank('tankblack', this, this.map, 1, 1, 45);
+        this.tankblack = new Tank('tankblack', this, this.map, 4, 4, 45);
         this.tankblue = new Tank('tankblue', this, this.map, 20, 1, 45);
         this.tankcyan = new Tank('tankcyan', this, this.map, 20, 20, 45);
         this.tankgreen = new Tank('tankgreen', this, this.map, 1, 10, 45);
@@ -45,7 +55,37 @@ class main extends Phaser.Scene {
         this.tankpurple = new Tank('tankpurple', this, this.map, 10, 20, 45);
         this.tankbred = new Tank('tankred', this, this.map, 10, 1, 45);
         this.tankyellow = new Tank('tankyellow', this, this.map, 20, 10, 45);
+
+        //scoreboard
+        this.nameText = this.add.text(10, 10, 'Name: Chickmagnet73', { font: '32px Arial', fill: '#000000' });
+        this.scoreText = this.add.text(10, 48, 'Score: 0', { font: '32px Arial', fill: '#000000' });
+        this.hitpointsText = this.add.text(10, 86, 'Hitpoints: 100', { font: '32px Arial', fill: '#000000' });
+
+        var explosion = {
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', { frames: [ 0, 1, 2, 3, 4 ] }),
+            frameRate: 5,
+            repeat: -1
+        };
+
+        var bullet = {
+            key: 'shot',
+            frames: this.anims.generateFrameNumbers('shot', { frames: [ 0, 1, 2, 3, 4 ] }),
+            frameRate: 5,
+            repeat: -1
+        };
+
+        this.anims.create(explosion);
+        this.anims.create(bullet);
+        this.add.sprite(300, 300, 'explosion').play('explode');
+        this.add.sprite(200, 200, 'southeastbullet').play('shot');
+
+        var explosion = game.add.sprite(200, 200, 'explosion');
+        var shoot = game.add.sprite(200, 200, 'shoot');
+
         this.map.selectTile(1,1);
+
+
     }
 
 /*     update(delta){
@@ -87,7 +127,7 @@ class main extends Phaser.Scene {
         this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-        //controls tankblue
+        //controls tankblack
         this.input.keyboard.on('keyup_Z', function(event){
             this.tankblack.forward();
         },this);
