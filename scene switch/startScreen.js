4f69d6dafe9f29b40159ff2e64dad9eeb487021a
jurne <jurne.decadt@gmail.com>
2019-03-15 13:49:58 +0100

    added loadscreen and changed startscreen

--- scene switch/startScreen.js
+++ scene switch/startScreen.js
@@ -8,12 +8,15 @@ class startScreen extends Phaser.Scene {
 
     create(){
         this.key_2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
-        this.text1 = this.add.text(2,2,"Press 2 to start the game", { font:"36px Impact"});
+        this.key_3 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.text1 = this.add.text(300,300,"Press on the spacebar to start the game", { font:"36px Impact"});
     }
 
     update(delta) {
         if(this.key_2.isDown){
             this.scene.start("main");
+        } else if(this.key_3.isDown){
+            this.scene.start("loadScreen");
         }
     }
 }
