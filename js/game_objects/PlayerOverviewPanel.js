45d6bc1a55deb373e799633fe2a8d8828d5ce887
Colin Pelkmans <colin.pelkmans@gmail.com>
2019-04-05 15:08:28 +0200

    fixed font size bug for scoreboard

--- js/game_objects/PlayerOverviewPanel.js
+++ js/game_objects/PlayerOverviewPanel.js
@@ -1,18 +1,19 @@
 class PlayerOverviewPanel extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, children) {
-        super(scene, x, y, children);
-        scene.add.existing(this);
+  constructor(scene, x, y, children) {
+    super(scene, x, y, children);
+    scene.add.existing(this);
 
-        let title = scene.add.text(5, 5, "Players:",
-          { fontSize: 48, font: 'Arial', fill: '#ff0000'}).setOrigin(0, 0);
-        this.add(title);    // Remove from scene and add to container
+    let title = scene.add.text(5, 5, "Players:",
+      { font: 'Arial', fill: '#ff0000' }).setOrigin(0, 0);
+    title.setFontSize(48);
+    this.add(title);    // Remove from scene and add to container
 
-        this.numberOfTanks = 0;
-    }
+    this.numberOfTanks = 0;
+  }
 
-    addPlayer(player, assetKey) {
-      let newY = (this.numberOfTanks > 0 ? this.last.y + 100 : 30);
-      this.add(new PlayerPanel(this.scene, 0, newY, null, player, assetKey));
-      this.numberOfTanks++;
-    }
+  addPlayer(player, assetKey) {
+    let newY = (this.numberOfTanks > 0 ? this.last.y + 100 : 30);
+    this.add(new PlayerPanel(this.scene, 0, newY, null, player, assetKey));
+    this.numberOfTanks++;
+  }
 }
--- js/game_objects/PlayerPanel.js
+++ js/game_objects/PlayerPanel.js
@@ -4,19 +4,19 @@ class PlayerPanel extends Phaser.GameObjects.Container {
         scene.add.existing(this);
 
         let nameLabel = scene.add.text(5, 5, playerName,
-            { fontSize: 300, font: "Arial", fill: "#FF0000" }).setOrigin(0, 0);
-
+            { font: "Arial", fill: "#FF0000" }).setOrigin(0, 0);
+        nameLabel.setFontSize(24);
         this.add(nameLabel);    // Remove from scene and add to container
-        
+
         let image = scene.make.image({
-          x: 5,
-          y: 20,
-          key: assetKey,
-          scale : {
-             x: 0.1,
-             y: 0.1
-          }
-        }).setOrigin(0,0);
+            x: 5,
+            y: 20,
+            key: assetKey,
+            scale: {
+                x: 0.1,
+                y: 0.1
+            }
+        }).setOrigin(0, 0);
         this.add(image);
     }
     // ...
