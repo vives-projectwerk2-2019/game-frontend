d4d4e5d700d7514eaa16e3d5e88fa92e7746ace3
Yoshi Hollez <yoshi.hollez@outlook.com>
2019-05-02 19:57:19 +0200

    fixed bug in hexmover that made mqtt disconnect and tanks not spawn

--- src/Mqtt/Mqtt.js
+++ src/Mqtt/Mqtt.js
@@ -1,10 +1,10 @@
-import { Client, Message } from "paho-mqtt"
+import { Client, Message } from "paho-mqtt";
 
 const mqtt_settings = {
   host: process.env.MQTT_BROKER_HOST || "game.bug.labict.be",
   port: Number(process.env.MQTT_BROKER_PORT) || 443,
   path: process.env.MQTT_BROKER_PATH || "/broker",
-  useSSL: (process.env.MQTT_BROKER_USE_SSL == "true") || true
+  useSSL: process.env.MQTT_BROKER_USE_SSL == "true" || true
 };
 
 /*jshint esversion: 6 */
@@ -13,10 +13,15 @@ class Mqtt {
     this.scene = scene;
     let mqtt = this;
     console.log(mqtt_settings);
-    this.client = new Client(mqtt_settings.host, mqtt_settings.port, mqtt_settings.path, "hell");
+    this.client = new Client(
+      mqtt_settings.host,
+      mqtt_settings.port,
+      mqtt_settings.path,
+      "hell"
+    );
 
-    this.arrayPlayers = [];
-    this.hasdied = [0];
+    mqtt.arrayPlayers = [];
+    mqtt.hasdied = [0];
 
     // set callback handlers
     this.client.onConnectionLost = function(responseObject) {
@@ -31,23 +36,21 @@ class Mqtt {
       // if (receivedMessage.commands.reset) {
       //   mqtt.scene.resetAllTanks();
       // }
-      if (receivedMessage.turn){
+      if (receivedMessage.turn) {
         scene.onNewRoundStarted(receivedMessage.turn);
       }
-
       for (let i = 0; i < receivedMessage.players.length; i++) {
         let username = receivedMessage.players[i].name;
         let player = receivedMessage.players[i];
-        if (!this.arrayPlayers.includes(username)) {
-          this.arrayPlayers.push(username);
+        if (!mqtt.arrayPlayers.includes(username)) {
+          mqtt.arrayPlayers.push(username);
           mqtt.scene.createTankSprite(player);
         } else {
-          mqtt.scene.HexMover.lerpTurn(player);
-          
+          mqtt.scene.setTankPosition(receivedMessage);
         }
-        if (player.tank.health <= 0 && !this.hasdied[i]) {
+        if (player.tank.health <= 0 && !mqtt.hasdied[i]) {
           mqtt.scene.destroyTank(username);
-          this.hasdied[i] = 1;
+          mqtt.hasdied[i] = 1;
         }
       }
     };
