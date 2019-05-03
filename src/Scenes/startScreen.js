0b5c29f0aaae32af49015df183784d324ac00587
Sille Van Landschoot <info@sillevl.be>
2019-05-03 09:47:41 +0200

    center text on startscreen

--- src/Scenes/startScreen.js
+++ src/Scenes/startScreen.js
@@ -13,8 +13,8 @@ class startScreen extends Phaser.Scene {
         this.key_2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
         this.key_3 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.input.on('pointerdown', () => this.scene.start("loadScreen"));
-
-        const clickButton = this.add.text(500, 400, "Press spacebar to start the game", { font: "72px Impact" })
+        const clickButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Press spacebar to start the game", { font: "72px Impact", align: "center" })
+        clickButton.setOrigin(0.5,0.5);
     }
 
     update(delta) {
