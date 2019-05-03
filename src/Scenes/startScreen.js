ead7e5ac29b865f006a82928b05101c27327e40e
Sille Van Landschoot <info@sillevl.be>
2019-05-03 09:58:05 +0200

    improve startScreen text

--- src/Scenes/startScreen.js
+++ src/Scenes/startScreen.js
@@ -13,8 +13,15 @@ class startScreen extends Phaser.Scene {
         this.key_2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
         this.key_3 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.input.on('pointerdown', () => this.scene.start("loadScreen"));
-        const clickButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Press spacebar to start the game", { font: "72px Impact", align: "center" })
+        const clickButton = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, "Press spacebar to start the game", 
+            { 
+                font: "72px Impact", 
+                align: "center",
+                padding: 10
+            }
+        );
         clickButton.setOrigin(0.5,0.5);
+        clickButton.setShadow(10, 10, 'black', 10);
     }
 
     update(delta) {
